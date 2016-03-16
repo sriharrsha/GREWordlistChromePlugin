e2a3f5eacf6c8c41f7f4b65bd273687ead26a402
Sri Harrsha <sriharrsha@live.com>
2016-03-16 11:14:34 +0530

    Added MutationObserver

--- gre.js
+++ gre.js
@@ -113,5 +113,26 @@ function greAdd(){
 	alert(div);
 }
 
+var observer = new MutationObserver(function(mutations) {
+    // For the sake of...observation...let's output the mutation to console to see how this all works
+    mutations.forEach(function(mutation) {
+        console.log(mutation.type);
+        alert(mutation.type);
+    });
+});
+
+// Notify me of everything!
+var observerConfig = {
+    attributes: true,
+    childList: true,
+    characterData: true
+};
+
+// Node, config
+// In this case we'll listen to all changes to body and child nodes
+var targetNode = document.body;
+observer.observe(targetNode, observerConfig);
+
+
 
 gre();
